dda8bfd3ed5c6f20401223c92606c10ea1e8cdf9
LucasHobold <lucash.santini@gmail.com>
2023-02-08 18:50:10 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,160 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      started: false,
+      ended: false,
+      isPaused: false,
+      recentlyPaused: false,
+      startDisabled: false,
+      numberOfSquares: 5,
+      timeToKill: 5,
+      numberOfLifes: 10,
+      lifesRemaining: 0,
+      minimunTimeToRespawn: 0,
+      squares: [],
+      score: 0,
+    };
+  },
+  watch: {
+    started(hasStarted) {
+      if (hasStarted) {
+        this.lifesRemaining = this.numberOfLifes;
+
+        for (let i = 0; i < this.numberOfSquares; i++) {
+          this.squares.push({
+            hasMonster: false,
+            spawning: false,
+            timeRemaining: this.timeToKill,
+            countdown: null,
+          });
+        }
+      }
+    },
+    squares: {
+      deep: true,
+      handler(newSquares) {
+        if (this.ended || !this.started || this.isPaused) {
+          return;
+        }
+
+        if (this.lifesRemaining <= 0) {
+          alertify.error("You lost");
+          this.endGame();
+
+          return;
+        }
+
+        for (const [i, s] of newSquares.entries()) {
+          if (!s.hasMonster && !s.spawning) {
+            this.spawnMonster(i);
+          } else if (s.hasMonster && s.timeRemaining <= 0) {
+            this.squares[i].hasMonster = false;
+            this.squares[i].timeRemaining = this.timeToKill;
+            this.lifesRemaining--;
+          }
+        }
+      },
+    },
+  },
+  computed: {
+    starColor() {
+      if (this.score > 200) {
+        return "#1c7ed6";
+      } else if (this.score > 100) {
+        return "#51cf66";
+      } else if (this.score > 50) {
+        return "#fcc419";
+      } else {
+        return "#fff";
+      }
+    },
+    pauseState() {
+      if (this.isPaused) {
+        return "Resume";
+      } else {
+        return "Pause";
+      }
+    },
+  },
+  methods: {
+    start() {
+      if (!this.numberOfSquares || !this.timeToKill) {
+        alertify.error("You need to fill in the amount of squares and the time to kill");
+        return;
+      }
+
+      this.startDisabled = true;
+
+      let timer;
+      let countdown = 2;
+
+      timer = setInterval(() => {
+        if (countdown <= 0) {
+          clearInterval(timer);
+          this.started = true;
+          return;
+        }
+
+        alertify.success(`The game will start in ${countdown}s`);
+        countdown--;
+      }, 1000);
+    },
+    spawnMonster(index, unPaused = false) {
+      this.squares[index].spawning = true;
+      let spawnTime = Math.floor((Math.random() * 5 + this.minimunTimeToRespawn) * 1000);
+
+      if (unPaused && this.squares[index].hasMonster) {
+        spawnTime = 0;
+      }
+
+      this.squares[index].spawner = setTimeout(() => {
+        this.squares[index].hasMonster = true;
+        this.squares[index].spawning = false;
+        this.squares[index].timeRemaining = unPaused ? this.squares[index].timeRemaining : this.timeToKill;
+        clearInterval(this.squares[index].countdown);
+
+        this.squares[index].countdown = setInterval(() => {
+          this.squares[index].timeRemaining--;
+        }, 1000);
+      }, spawnTime);
+    },
+    killMonster(index) {
+      if (!this.squares[index].hasMonster) {
+        this.lifesRemaining--;
+        alertify.error("No monster");
+      }
+
+      this.score++;
+      this.squares[index].hasMonster = false;
+    },
+    endGame() {
+      this.ended = true;
+      this.started = false;
+
+      for (let i = 0; i < this.squares.length; i++) {
+        clearInterval(this.squares[i].countdown);
+        clearTimeout(this.squares[i].spawner);
+      }
+    },
+    restart() {
+      this.started = false;
+      this.ended = false;
+      this.startDisabled = false;
+      this.squares = [];
+    },
+    togglePause() {
+      for (let i = 0; i < this.squares.length; i++) {
+        if (!this.isPaused) {
+          clearInterval(this.squares[i].countdown);
+          clearTimeout(this.squares[i].spawner);
+        } else {
+          this.spawnMonster(i, true);
+        }
+      }
+
+      this.isPaused = !this.isPaused;
+    },
+  },
+});
+
+app.mount("#app");
